f1317b89e5cd6bef1d910fcb301a2a5df3e8f253
42nado <daryllfortunado@gmail.com>
2022-10-09 17:07:33 +0800

    add not found route

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const {render} = require('ejs');
 const app= express();
 const path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>{
     res.render('index')
+})
+
+app.get('*', (req,res) =>{
+    res.render('notfound')
 })
 
 
