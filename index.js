7e037c1acf5c245033ba9090a49edaf6fc16d7d5
42nado <daryllfortunado@gmail.com>
2022-10-12 21:32:08 +0800

    add detail route

--- index.js
+++ index.js
@@ -7,12 +7,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) =>{
-    res.render('index')
+app.get('/home', (req, res) =>{
+    res.render('index');
+})
+
+app.get('/login', (req,res) =>{
+    res.render('login');
+})
+
+app.get('/home/details', (req,res) =>{
+    res.render('details');
 })
 
 app.get('*', (req,res) =>{
-    res.render('notfound')
+    res.render('notfound');
 })
 
 
