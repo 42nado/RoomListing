3378893ab45bd752b435e0639affb931e49e1c98
42nado <daryllfortunado@gmail.com>
2023-06-27 11:05:36 +0800

    update port

--- index.js
+++ index.js
@@ -13,30 +13,18 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const session = require('express-session');
 //use .env file to store api key
 require('dotenv').config();
-const port = process.env.PORT || 3000;
-// const mongoURI = process.env.MONGO_URI;
+const port = process.env.PORT || 5000;
 
 // require('mongoose-type-url');
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-      console.log('Mongo Connection Open!');
+        console.log('Mongo Connection Open!');
     })
     .catch(err => {
-      console.log('Mongo Connection Error!');
-      console.log(err);
-    });
-// mongoose.connect('mongodb://localhost:27017/mylisting')
-//     .then(() => {
-//         console.log('Mongo Connection Open!');
-//     })
-//     .catch(err => {
-//         console.log('Mongo Connection Error!');
-//         console.log(err);
-//     })
+        console.log('Mongo Connection Error!');
+        console.log(err);
+    })
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -271,5 +259,5 @@ app.get('*', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log("Server started on port 3000!")
+    console.log("Server started on port " + port)
 })
